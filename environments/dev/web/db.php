67fe2ca25ec400706d224e10c3d123ec3f844c18
Tosha <toha.stepanyuck@gmail.com>
2023-04-12 23:14:15 +0300

    Инициализация 2.0

---
+++ environments/dev/web/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'pgsql:host=localhost;port=5432;dbname=postgres',
+    'username' => 'test_user',
+    'password' => 'test_password',
+    'charset' => 'utf8',
+];
