f2e7afb68b1d00115c489ad80c5a6a8d5f2a13ca
Tosha <toha.stepanyuck@gmail.com>
2023-04-22 13:25:54 +0300

    Добавление зашифрованного пароля

--- config/db.php
+++ config/db.php
@@ -1,9 +1,18 @@
 <?php
 
+$username = 'test_user';
+$enpass = '8UKJbFalRlbK/LoCkFQ28Q==';
+
+$key = "5aa3c281e42ba7101f7227a7519d5e961c7bcf2b10a42914304bffc1afcebb1d2be98f53caa80d05";
+$method = "AES-192-CBC";
+$iv = 5782920193848291;
+$pass = openssl_decrypt($enpass, $method, $key, 0, $iv);
+
+
 return [
     'class' => 'yii\db\Connection',
     'dsn' => 'pgsql:host=calc-postgresdb;port=5432;dbname=postgres',
-    'username' => 'test_user',
-    'password' => 'test_password',
+    'username' => $username,
+    'password' => $pass,
     'charset' => 'utf8',
 ];
